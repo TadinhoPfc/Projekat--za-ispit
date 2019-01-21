c5f8dc95b2950fdd33a84838e00261b093a28fb0
DzoniPfc <dzoni2796@live.com>
2019-01-21 21:54:29 +0100

    sdasda

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,24 @@
 console.log("okej");
 
-
+$("#form-style").submit(function(e) {
+    e.preventDefault();
+});
 function provera(){
-console.log("Radi");
+  console.log("Radi");
+  var kola = document.getElementById("car-select");
   var ime = document.getElementById("ime");
+  var email = document.getElementById("email");
+  var broj = document.getElementById("broj");
   var imeSadr = ime.value;
+  var emailSadr = email.value;
+  var brojSadr = parseInt(broj.value);
+
   console.log("Uneto ime je : "+imeSadr);
+  console.log("Uneti E-mail je : "+emailSadr);
+  if( !isNaN(brojSadr) && brojSadr!=null ){
+  console.log("Uneti broj telefona je : "+brojSadr);
+} else {
+  console.log("Nije uneta ispravna vrednost!");
+}
 
 };
