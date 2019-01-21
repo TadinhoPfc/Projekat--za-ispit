faf8b3d91363b5b4a0cf7c5f0714ade4c1b7ea13
TadinhoPfc <tadija.milanovic.18@singimail.rs>
2019-01-21 18:36:30 +0100

    Sajt

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+console.log("okej");
+
+
+function provera(){
+console.log("Radi");
+  var ime = document.getElementById("ime");
+  var imeSadr = ime.value;
+  console.log("Uneto ime je : "+imeSadr);
+
+};
